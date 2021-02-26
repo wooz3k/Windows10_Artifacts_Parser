397f49630d58e2250957f63d0faa9f5704a55f36
wooz3k <70104464+wooz3k@users.noreply.github.com>
2021-02-27 03:10:21 +0900

    Update lnk.py

--- lnk.py
+++ lnk.py
@@ -15,46 +15,50 @@ def Win_ts(timestamp):
     WIN32_EPOCH = datetime(1601, 1, 1)
     return WIN32_EPOCH + timedelta(microseconds=timestamp//10, hours=9)
 
-Dir_path=sys.argv[1]
-LNK_file_list =  glob.glob(Dir_path+'/*.lnk')
-LNK_file_Name_List = os.listdir(Dir_path)
+LDir_path="C:/Users/정우성/Desktop/lnktest"
+LNK_file_list =  ""
+LNK_file_Name_List = ""
 
 LNK_File_Name, LNK_Creation_Time, LNK_File_Created_Time, LNK_Access_Time, LNK_Write_Time, LNK_File_Size = [],[],[],[],[],[]
+LNK_Data=[]
 
-f = open('LNK_info.csv', 'w', encoding='utf-8', newline='')
+def lnk_parser():
+    cut = 0
+    LNK_file_list =  glob.glob(LDir_path+'/*.lnk')
+    LNK_file_Name_List = os.listdir(LDir_path)
 
-wr = csv.writer(f)
-wr.writerow(["LNK_File_Name", "Creation_Time", "Created_Time" ,"Access_Time", "Write_Time", "File_Size"])
+    for LNK_file in LNK_file_list:
+        Data = raw_data(LNK_file)
+        LNK_File_Name = LNK_file_Name_List
+        LNK_Creation_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x1C:])[0]).strftime('%Y:%m:%d-%H:%M:%S.%f'))
+        LNK_File_Created_Time.append(datetime.fromtimestamp(os.path.getctime(LNK_file)).strftime('%Y:%m:%d-%H:%M:%S.%f'))
+        LNK_Access_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x24:])[0]).strftime('%Y:%m:%d-%H:%M:%S.%f'))
+        LNK_Write_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x2C:])[0]).strftime('%Y:%m:%d-%H:%M:%S.%f'))
+        LNK_File_Size.append(struct.unpack_from("<i",(Data[0x34:]))[0])
+        
+        LNK_Data.append((LNK_File_Name[cut], LNK_Creation_Time[cut], LNK_File_Created_Time[cut], LNK_Access_Time[cut], LNK_Write_Time[cut], LNK_File_Size[cut]))
+        
+        print('[*]File_Name :'+ str(LNK_File_Name[cut]))
+        print('[**]Creation_Time :' + str(LNK_Creation_Time[cut]))
+        print('[***]Created_Time :' + str(LNK_File_Created_Time[cut]))
+        print('[****]Access_Time :' + str(LNK_Access_Time[cut]))
+        print('[*****]Write_Time : '+ str(LNK_Write_Time[cut]))
+        print('[*****]File_Size : ' + str(LNK_File_Size[cut])+'byte')
+        print('\n')
+        cut +=1
 
-cut = 0
+    return len(LNK_file_list)
 
-for LNK_file in LNK_file_list:
-    Data = raw_data(LNK_file)
-    LNK_File_Name = LNK_file_Name_List
-    LNK_Creation_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x1C:])[0]).strftime('%Y:%m:%d-%H:%M:%S.%f'))
-    LNK_File_Created_Time.append(datetime.fromtimestamp(os.path.getctime(LNK_file)).strftime('%Y:%m:%d-%H:%M:%S.%f'))
-    LNK_Access_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x24:])[0]).strftime('%Y:%m:%d-%H:%M:%S.%f'))
-    LNK_Write_Time.append(Win_ts(struct.unpack_from("<Q", Data[0x2C:])[0]).strftime('%Y:%m:%d-%H:%M:%S.%f'))
-    LNK_File_Size.append(struct.unpack_from("<i",(Data[0x34:]))[0])
-    
-    print('[*]File_Name :'+ str(LNK_File_Name[cut]))
-    print('[**]Creation_Time :' + str(LNK_Creation_Time[cut]))
-    print('[***]Created_Time :' + str(LNK_File_Created_Time[cut]))
-    print('[****]Access_Time :' + str(LNK_Access_Time[cut]))
-    print('[*****]Write_Time : '+ str(LNK_Write_Time[cut]))
-    print('[*****]File_Size : ' + str(LNK_File_Size[cut])+'byte')
-    print('\n')
-    
-    cut +=1
-		
+def lnk_output_csv(path):	
 
-cut = 0
+    LNK_file_list =  glob.glob(LDir_path+'/*.lnk')
+    LNK_file_Name_List = os.listdir(LDir_path)
 
-for LNK in LNK_file_Name_List:
-    if LNK.endswith('.lnk'):
-        wr.writerow([LNK_File_Name[cut], LNK_Creation_Time[cut], LNK_File_Created_Time[cut], LNK_Access_Time[cut], LNK_Write_Time[cut], str(LNK_File_Size[cut])+'byte'])
-        cut+=1
+    f = open(path+'/LNK_info.csv', 'w', encoding='utf-8', newline='')
 
-f.close()
+    wr = csv.writer(f)
+    wr.writerow(["LNK_File_Name", "Creation_Time", "Created_Time" ,"Access_Time", "Write_Time", "File_Size"])
 
-print('Finished')
+    for i, (name, creation_time, created_time, access_time, write_time, size) in enumerate(LNK_Data):
+            wr.writerow([name, creation_time, created_time, access_time, write_time, str(size)+'byte'])
+    f.close()
